c3a3df9d378d725ffc8b27c25c4e120d5be64c56
TheRealDeathEye <therealdeatheye@gmail.com>
2022-12-01 17:33:18 -0600

    mm-update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,36 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
+import { getAccount } from './Accounts';
+import { XrpClient } from './XrpClient';
+import { Client } from 'xrpl';
+import { TransactionBuilder } from './TransactionBuilder';
 
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
+  
+  const userAccount = await  getAccount(2);
+  let network : "testnet" | "mainnet" = "mainnet";
+  if(request.params && request.params['network']){
+    if(request.params['network'] === "testnet" || request.params['network'] === "mainnet")
+      network = request.params['network']
+  }
+  const client = new XrpClient(network);
   switch (request.method) {
-    case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+    case 'getAddress':
+      return userAccount.address
+    case 'getAccountInfo':
+      return await client.getAccountInfo(userAccount.address)
+    case 'getBalance':
+      return await client.getBalance(userAccount.address);
+    case 'signTxn':
+      return 'yes'
+    case 'transfer':
+      console.log("called");
+      const txn = await TransactionBuilder.buildPaymentXrp(client, userAccount.address, request.params.to, request.params.amount);
+      const signed = userAccount.sign(txn);
+      await client.submit(signed.tx_blob);
+      return signed.hash;
+      
     default:
       throw new Error('Method not found.');
+      
   }
 };
